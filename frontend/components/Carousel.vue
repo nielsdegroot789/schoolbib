f2834a1fda1bed977f1585ae0e0a3d271d745d13
Your Name <nielsdegroot@live.be>
2020-09-01 18:11:21 +0200

    installed font awesome

--- frontend/components/Carousel.vue
+++ frontend/components/Carousel.vue
@@ -1,19 +1,33 @@
 <template>
   <div class="carousel-container">
-    <button class="carousel-button"><i class="fas fa-arrow-left"></i></button>
+    <button class="carousel-button">
+      <font-awesome-icon :icon="['fas', 'angle-left']" />
+    </button>
+    <div v-for="(item, id) in bookMeta" :key="id" class="carousel-book">
+      {{ item.title }}
+    </div>
     <div class="carousel-book"></div>
     <div class="carousel-book"></div>
     <div class="carousel-book"></div>
     <div class="carousel-book"></div>
     <div class="carousel-book"></div>
     <div class="carousel-book"></div>
-    <div class="carousel-book"></div>
-    <button class="carousel-button"><i class="fas fa-arrow-right"></i></button>
+    <button class="carousel-button">
+      <font-awesome-icon :icon="['fas', 'angle-right']" />
+    </button>
   </div>
 </template>
 <script>
 export default {
   name: 'Carousel',
+  data() {
+    return {};
+  },
+  computed: {
+    bookMeta() {
+      return this.$store.state.bookMeta;
+    },
+  },
 };
 </script>
 <style></style>
