9622ca7d1f1fe21ab406becf1187065fd9adabfa
emieldkr <emieldkr@gmail.com>
2020-09-09 14:38:21 +0200

    edit form

--- frontend/pages/book/EditForm.vue
+++ frontend/pages/book/EditForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <BookForm v-if="ready" :travelData="travelData" />
+    <BookForm v-if="ready" :bookData="bookData" />
   </div>
 </template>
 
@@ -10,33 +10,34 @@ export default {
   name: 'EditForm',
   components: { BookForm },
   created() {
-    this.$root.directusClient
-      .getItems('travel', {
-        filter: {
-          id: {
-            eq: this.$route.params.id,
-          },
-        },
-        fields: '*.*',
-      })
-      .then(
-        function (result) {
-          if (!result.data) {
-            throw new Error('Could not find travel');
-          }
-          this.bookData = result.data[0];
-          this.ready = true;
-        }.bind(this), // if this is not bound you can not access this in the callback (you could also use an arrow function
-      )
-      .catch(
-        function (error) {
-          console.error(error);
-          this.$root.$emit('notify', {
-            title: 'Travel not found',
-            body: `Travel with id ${this.$root.params.id} not found`,
-          });
-        }.bind(this),
-      );
+    //todo this needs to be remade with actual backend
+    // this.$root.directusClient
+    //   .getItems('travel', {
+    //     filter: {
+    //       id: {
+    //         eq: this.$route.params.id,
+    //       },
+    //     },
+    //     fields: '*.*',
+    //   })
+    //   .then(
+    //     function (result) {
+    //       if (!result.data) {
+    //         throw new Error('Could not find travel');
+    //       }
+    //       this.bookData = result.data[0];
+    //       this.ready = true;
+    //     }.bind(this), // if this is not bound you can not access this in the callback (you could also use an arrow function
+    //   )
+    //   .catch(
+    //     function (error) {
+    //       console.error(error);
+    //       this.$root.$emit('notify', {
+    //         title: 'Travel not found',
+    //         body: `Travel with id ${this.$root.params.id} not found`,
+    //       });
+    //     }.bind(this),
+    //   );
   },
   data() {
     return {
