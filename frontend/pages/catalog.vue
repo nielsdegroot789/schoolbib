a288cd4fa06a0e9ef95a79e93012133f791e7e1a
benno <benno.demey@outlook.com>
2020-09-12 15:24:54 +0200

    stuck a lot

---
+++ frontend/pages/catalog.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id='myapp'>
+
+    <input type='text' v-model='bookName' placeholder="Enter a name of a book">
+
+    <input type='button' @click='allBooks()' value='Select all books'>
+    <br><br>
+
+    <table>
+        <tr>
+        <th>title</th>
+        <th>Rating</th>
+        <th>Email</th>
+        </tr>
+
+        <tr v-for='books in MetaBook'>
+        <td>{{ MetaBook.title }}</td>
+        <td>{{ MetaBook.name }}</td>
+        <td>{{ MetaBook.email }}</td>
+        </tr>
+    </table>
+ 
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>template>
