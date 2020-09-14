05344c9d297480b0ece8e3a4bb1d0f487831ddd9
emieldkr <emieldkr@gmail.com>
2020-09-14 20:19:16 +0200

    deleted unused routes

--- backend/src/public/index.php
+++ backend/src/public/index.php
@@ -34,34 +34,12 @@ $app = AppFactory::create();
 // Add Twig-View Middleware
 $app->add(TwigMiddleware::createFromContainer($app));
 
-// $app->map(['GET', 'POST'], '/login', \skoolBiep\Controller\UserController::class . ':login');
-
-// $app->map(['GET', 'POST'], '/create', \skoolBiep\Controller\UserController::class . ':create');
+//routes
 $app->get('/getBookMeta', \skoolBiep\Controller\BookController::class . ':getBookMeta');
 
 $app->get('/getBooks', \skoolBiep\Controller\BookController::class . ':getBooks');
 
 $app->post('/saveBook', \skoolBiep\Controller\BookController::class . ':saveBook');
-
-
-  
-// $app->map(['GET', 'POST'], '/create', function (Request $request, Response $response, array $args) {
-//     $this->get('db');
-//     if ($request->getMethod() == 'GET') {
-//         $html = '<h1>Login</h1>
-// <form method="POST"><label for="username">Username:</label><input type="text" size="40" name="username" /><br />';
-//         $html = $html . '<label for="password">Password:</label><input type="password" size="40" name="password" /><br />';
-//         $html = $html . '<input type="submit" value="Create user" name="Save" /></form>';
-//         $response->getBody()->write($html);
-//     } else {
-//         // verwerking form
-//         $user = new User();
-//         $user->setUserName($request->getParsedBody()['username']);
-//         $id = $user->saveUser($request->getParsedBody()['password']);
-//         echo "User received ID: $id";
-//     }
-//     return $response;
-// });
 
 $app->run();
  
