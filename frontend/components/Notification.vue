1dd657847e9c0c8671cf8a48ad1abe567047ac40
Your Name <nielsdegroot@live.be>
2020-09-24 13:08:25 +0200

    styling notify

--- frontend/components/Notification.vue
+++ frontend/components/Notification.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="notify-container">
-    <button class="close-button">Close</button>
-    <h2>tekst</h2>
+    <button class="close-button">
+      <font-awesome-icon :icon="['fa', 'times']" />
+    </button>
+    <h2 class="notification-text">Changes saved</h2>
   </div>
 </template>
 
@@ -14,11 +16,21 @@ export default {
 <style>
 .notify-container {
   position: fixed;
+  background-color: grey;
+  width: 200px;
+  height: 100px;
   bottom: 1rem;
   right: 3rem;
   z-index: 5000;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
 }
 .close-button {
-  max-width: initial;
+  max-width: 100px;
+}
+.notification-text {
+  margin-top: 1rem;
+  align-self: center;
 }
 </style>
