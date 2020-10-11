91533c17aeb107d5173408555385abb005e35e87
Niels De Groot <nielsdegroot@live.be>
2020-10-11 21:53:35 +0200

    editbookmodal

--- frontend/components/AdminEditBook.vue
+++ frontend/components/AdminEditBook.vue
@@ -1,29 +1,33 @@
 <template>
   <div>
+    <editBookModal />
     <div class="section box">
-      <div
-        v-for="item in adminSpecificBooks"
-        :key="item.id"
-        class="div level-box"
-      >
-        <h1>id : {{ item.id }}</h1>
-        <h1>status: {{ item.status }}</h1>
-        <button>Edit</button>
-        <button @click="showModal">Delete</button>
+      <div>
+        <div
+          v-for="item in adminSpecificBooks"
+          :key="item.id"
+          class="div level-box"
+        >
+          <h1>id : {{ item.id }}</h1>
+          <h1>status: {{ item.status }}</h1>
+          <button>Edit</button>
+          <button @click="showModal">Delete</button>
+        </div>
       </div>
-      <button @click="newBook">new</button>
+      <button>New</button>
     </div>
+
     <div :class="['modal', { 'is-active': active }]">
       <div class="modal-background" @click="showModal"></div>
-      <div class="modal-card">
+      <div class="modal-card card-width">
         <header class="modal-card-head">
           <p class="modal-card-title">Do you want to delete this book?</p>
-          <button class="delete" aria-label="close"></button>
+          <button class="delete" aria-label="close" @click="showModal"></button>
         </header>
-        <section class="modal-card-body">
+        <section class="modal-card-body card-body">
           <button
             class="button is-danger is-halfwidth"
-            @click="deleteSpecificBook(item.id)"
+            @click="deleteSpecificBook(id)"
           >
             Delete this book
           </button>
@@ -35,8 +39,12 @@
 </template>
 
 <script>
+import editBookModal from '../components/EditNewBookModal';
 export default {
   name: 'AdminEditBook',
+  components: {
+    editBookModal,
+  },
   data() {
     return {
       active: false,
@@ -63,10 +71,10 @@ export default {
 </script>
 
 <style>
-.modal-card {
-  max-width: 32%;
+.card-width {
+  max-width: 35%;
 }
-.modal-card-body {
+.card-body {
   display: flex;
   justify-content: space-around;
 }
---
+++ frontend/components/EditNewBookModal.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="modal is-active">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">book</p>
+        <button class="delete" aria-label="close"></button>
+      </header>
+      <section class="modal-card-body">
+        <FormulateForm v-model="formValues">
+          <FormulateInput type="text" name="id" label="id" />
+          <FormulateInput type="text" name="stock" label="Stock" />
+          <FormulateInput type="text" name="qr-code" label="qr-code" />
+          <FormulateInput type="text" name="status" label="status" />
+        </FormulateForm>
+      </section>
+      <footer class="modal-card-foot">
+        <button class="button is-success" @click="sendChanges">
+          Save changes
+        </button>
+        <button class="button">Cancel</button>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EditNewBook',
+  data() {
+    return {
+      formValues: '',
+    };
+  },
+  methods: {
+    sendChanges() {
+      this.$axios.put(
+        'http://localhost:8000/handleSpecificBook',
+        this.formValues,
+      );
+    },
+  },
+};
+</script>
+
+<style></style>
