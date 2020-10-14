b5f499c8ddf0bae05d00160692142fca98964907
Niels De Groot <nielsdegroot@live.be>
2020-10-14 15:50:15 +0200

    fix dynamic class hidden

--- frontend/components/bookFormData.vue
+++ frontend/components/bookFormData.vue
@@ -3,15 +3,14 @@
     <div class="modal-background" @click="closeModal"></div>
     <div class="modal-card">
       <header class="modal-card-head">
-        <p :class="['modal-card-title', { hidden: book.id }]">Add book</p>
-        <p :class="['modal-card-title', { hidden: !book.id }]">Update book</p>
+        <p :class="['modal-card-title', { hidden: !book.empty }]">Add book</p>
+        <p :class="['modal-card-title', { hidden: book.empty }]">Update book</p>
         <button class="delete" aria-label="close" @click="closeModal"></button>
-        {{ id }}
       </header>
       <section class="modal-card-body">
         <div class="field">
           <div class="control">
-            <FormulateForm v-model="book">
+            <FormulateForm v-model="formValues">
               <FormulateInput type="text" name="id" label="id" />
               <FormulateInput type="text" name="stock" label="Stock" />
               <FormulateInput type="text" name="qrCode" label="qr-code" />
@@ -22,13 +21,13 @@
       </section>
       <footer class="modal-card-foot">
         <button
-          :class="['button', 'is-success', { hidden: !book.id }]"
+          :class="['button', 'is-success', { hidden: book.empty }]"
           @click="sendChanges"
         >
           Save changes
         </button>
         <button
-          :class="['button', 'is-success', { hidden: book.id }]"
+          :class="['button', 'is-success', { hidden: !book.empty }]"
           @click="newBook"
         >
           Add book
@@ -41,11 +40,28 @@
 <script>
 export default {
   name: 'BookFormData',
-  props: ['book'],
-
+  props: {
+    book: {
+      type: Object,
+      default() {
+        return { empty: true };
+      },
+      required: true,
+    },
+  },
+  data() {
+    return {
+      formValues: this.book,
+    };
+  },
   computed: {
     editModal() {
       return this.$store.state.editModal;
+    },
+  },
+  watch: {
+    book(book) {
+      this.formValues = book;
     },
   },
   methods: {
