4dd5d3a75787708ef566038e97e23e624cb2cf67
benno <benno.demey@outlook.com>
2020-10-15 20:40:23 +0200

    solution page system

---
+++ frontend/pages/book/_page.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <Pagination />
+  </div>
+</template>
+
+<script>
+import Pagination from '~/components/Pagination'
+export default {
+    components: 
+    Pagination,
+}
+</script>
+
+<style>
+
+</style>
