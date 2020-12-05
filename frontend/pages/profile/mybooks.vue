5ca2af1ce60a4d031d2bed6b1defdf14a10d2b79
benno <benno.demey@outlook.com>
2020-12-06 00:14:20 +0100

    whishlist

--- frontend/pages/profile/mybooks.vue
+++ frontend/pages/profile/mybooks.vue
@@ -1,7 +1,57 @@
 <template>
   <div>
-    <h1>Book list is empty...</h1>
-    <h1>Add some books</h1>
+    <h1>this is the list of books that you vhevked out and reserved</h1>
+    <h2>My reserved Books</h2>
+    <table class="table table is-bordered is-hoverable is-fullwidth">
+      <thead>
+        <tr>
+          <th>id_user</th>
+          <th>name_user</th>
+          <th>id_book</th>
+          <th>name_book</th>
+          <th>reservation date</th>
+          <th>Accept</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in reservations" :key="index">
+          <td>{{ item.usersId }}</td>
+          <td>{{ item.usersName }}</td>
+          <td>{{ item.booksId }}</td>
+          <td>{{ item.booksName }}</td>
+          <td>{{ item.reservationDateTime }}</td>
+          <td class="checkoutBtn" @click="saveCheckout(item)">delete</td>
+        </tr>
+      </tbody>
+    </table>
+    <h2>Checkouts</h2>
+
+    <table class="table table is-bordered is-hoverable is-fullwidth">
+      <thead>
+        <tr>
+          <th>userName</th>
+          <th>book</th>
+          <th>checkoutDateTime</th>
+          <th>maxAllowedDate</th>
+          <th>fine</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in checkouts" :key="index">
+          <td>{{ item.usersName }}</td>
+          <td>{{ item.booksName }}</td>
+          <td>{{ item.checkoutDateTime }}</td>
+          <td>{{ item.maxAllowedDate }}</td>
+          <td>{{ item.fine }}</td>
+          <td
+            class="checkoutBtn"
+            @click="returnCheckouts(item, index) in checkouts"
+          >
+            delete
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
--- frontend/pages/profile/wishlist.vue
+++ frontend/pages/profile/wishlist.vue
@@ -2,45 +2,16 @@
   <div class="setup section">
     <header class="level">
       <h1 class="level-left title">
-        this is the wishlist. of books you have added as favorite
+        Wishlits. this could be a carousel with books that are you favorite.
       </h1>
     </header>
-    <h2>Reservations</h2>
-    <table class="table table is-bordered is-hoverable is-fullwidth">
-      <thead>
-        <tr>
-          <th>id_user</th>
-          <th>name_user</th>
-          <th>id_book</th>
-          <th>name_book</th>
-          <th>reservation date</th>
-          <th>Accept</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(item, index) in reservations" :key="index">
-          <td>{{ item.usersId }}</td>
-          <td>{{ item.usersName }}</td>
-          <td>{{ item.booksId }}</td>
-          <td>{{ item.booksName }}</td>
-          <td>{{ item.reservationDateTime }}</td>
-          <td class="checkoutBtn" @click="saveCheckout(item)">Accept!</td>
-        </tr>
-      </tbody>
-    </table>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-      reservations: '',
-      checkouts: '',
-      fine: '',
-      isEditing: false,
-      DateNow: '',
-    };
+    return {};
   },
   computed: {},
 
