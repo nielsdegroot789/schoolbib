cf9cf94dc1ab2203b610a3e7f2df924ab76825bb
benno <benno.demey@outlook.com>
2020-12-08 15:02:23 +0100

    css fix

--- frontend/pages/profile/wishlist.vue
+++ frontend/pages/profile/wishlist.vue
@@ -20,7 +20,7 @@
     <thead>
       <tr>
         <th>Authors U like</th>
-        <th>delete</th>
+        <th class="deleteButton">delete</th>
       </tr>
     </thead>
     <tbody>
@@ -80,9 +80,23 @@ export default {
         this.dataFavBook = response.data;
       });
   },
-
-  methods: {},
 };
 </script>
 
-<style></style>
+<style>
+.deleteButton {
+  display: inline-block;
+  padding: 0.7em 1.4em;
+  margin: 0 0.3em 0.3em 0;
+  border-radius: 0.15em;
+  box-sizing: border-box;
+  text-decoration: none;
+  font-family: 'Roboto', sans-serif;
+  text-transform: uppercase;
+  color: #ffffff;
+  background-color: #3369ff;
+  box-shadow: inset 0 -0.6em 0 -0.35em rgba(0, 0, 0, 0.17);
+  text-align: center;
+  position: relative;
+}
+</style>
