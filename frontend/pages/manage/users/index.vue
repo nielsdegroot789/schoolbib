4ae4d91fea7a552f4d9f8f552c04a5eafa8b6139
emieldkr <emieldkr@gmail.com>
2020-12-14 13:08:39 +0100

    setting up manage users

--- frontend/pages/manage/users/index.vue
+++ frontend/pages/manage/users/index.vue
@@ -1,13 +1,113 @@
 <template>
-  <div class="setup section">This is page for managing the actual users</div>
+  <div class="setup section">
+    <header class="level">
+      <h1 class="level-left title">Manage Users</h1>
+      <nuxt-link :to="{ path: '/manage/users/new' }" class="button level-right"
+        >new</nuxt-link
+      >
+    </header>
+    <table class="table table is-bordered is-hoverable is-fullwidth">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Surname</th>
+          <th>Last name</th>
+          <th>age</th>
+          <th>email</th>
+          <th>send password reset</th>
+          <th>edit</th>
+          <th>delete</th>
+        </tr>
+      </thead>
+      <tfoot>
+        <tr>
+          <th>Id</th>
+          <th>Surname</th>
+          <th>Last name</th>
+          <th>age</th>
+          <th>email</th>
+          <th>send password reset</th>
+          <th>edit</th>
+          <th>delete</th>
+        </tr>
+      </tfoot>
+      <tbody>
+        <tr v-for="book in bookMeta" :key="book.id">
+          <td>{{ book.id }}</td>
+          <td>{{ book.surname }}</td>
+          <td>{{ book.lastname }}</td>
+          <td>{{ book.age }}</td>
+          <td>{{ book.email }}</td>
+          <td>
+            <button class="button">send PW reset</button>
+          </td>
+          <td>
+            <nuxt-link
+              :to="{ path: '/manage/users/edit/' + book.id }"
+              class="button"
+            >
+              edit
+            </nuxt-link>
+          </td>
+          <td>
+            <button class="button">delete</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
 export default {
-  created() {
-    this.$router.push('/manage/users/1');
+  data() {
+    return {
+      users: [],
+    };
+  },
+  computed: {},
+  mounted() {
+    this.$axios
+      .get('http://localhost:8080/getUsers', {
+        headers: { Auth: this.$store.JWT },
+      })
+      .then((response) => {
+        this.users = response.data;
+      });
   },
 };
 </script>
 
-<style></style>
+<style>
+.setup {
+  margin: 0 5%;
+  box-sizing: border-box;
+  height: 100%;
+  overflow-x: auto;
+}
+
+.links {
+  padding-top: 15px;
+}
+
+.booksContainer:nth-child(odd) {
+  background-color: lightgray;
+}
+
+.headerContainer {
+  display: grid;
+  grid-template-columns: repeat(11, calc(90% / 11)) 5% 5%;
+  justify-items: center;
+  align-items: center;
+  margin: 5px 0;
+  font-weight: bold;
+}
+
+.booksContainer {
+  display: grid;
+  grid-template-columns: repeat(11, calc(90% / 11)) 5% 5%;
+  justify-items: center;
+  align-items: center;
+  margin: 5px 0;
+}
+</style>
