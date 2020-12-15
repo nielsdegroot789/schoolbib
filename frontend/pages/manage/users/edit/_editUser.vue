7884105df5618c6e468b81fbefa7a5879ac851f7
emieldkr <emieldkr@gmail.com>
2020-12-15 09:46:18 +0100

    editUser

---
+++ frontend/pages/manage/users/edit/_editUser.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <UserForm :user-data="userMeta" @updateUserData="updateUserData" />
+  </div>
+</template>
+
+<script>
+import UserForm from '~/components/UserForm.vue';
+export default {
+  name: 'EditForm',
+  components: { UserForm },
+  data() {
+    return {
+      ready: false,
+      userMeta: {},
+    };
+  },
+  mounted() {
+    this.$axios
+      .get('http://localhost:8080/getUserFromId', {
+        params: { id: this.$route.params.editUser },
+        headers: {
+          Auth: this.$store.state.JWT,
+        },
+      })
+      .then((response) => {
+        this.userMeta = response.data[0];
+      });
+  },
+  methods: {
+    updateUserData(newDataObj) {
+      this.userData = newDataObj;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.edit-event-page {
+  padding: 2rem 4rem;
+}
+</style>
